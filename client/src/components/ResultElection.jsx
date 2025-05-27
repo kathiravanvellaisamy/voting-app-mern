40b16fea7a53dc7a28d37835bd2bdd600ee5f301
kathiravan vellaisamy <kathiravanvellaisamy@gmail.com>
2025-05-27 20:39:39 +0530

    edited hyper link

--- client/src/components/ResultElection.jsx
+++ client/src/components/ResultElection.jsx
@@ -26,7 +26,7 @@ const ResultElection = ({ id, thumbnail, title }) => {
           />
         ))}
       </ul>
-      <Link to={`/election/${id}/candidates`} className="btn primary full">
+      <Link to={`/elections/${id}/candidates`} className="btn primary full">
         Enter Election
       </Link>
     </article>
